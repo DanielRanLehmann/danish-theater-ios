90d51e0a2a7a97fa32f51a819c87ee6bb881ee79
Daniel Ran Lehmann <danielran11@gmail.com>
2017-06-26 18:12:48 +0200

    Initial Commit

---
+++ Danish Theater/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Danish Theater
+//
+//  Created by Daniel Ran Lehmann on 6/26/17.
+//  Copyright © 2017 Daniel Ran Lehmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Danish Theater/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Danish Theater
+//
+//  Created by Daniel Ran Lehmann on 6/26/17.
+//  Copyright © 2017 Daniel Ran Lehmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
